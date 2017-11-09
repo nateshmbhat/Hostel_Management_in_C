ff3dde0ffdfee623854634a3011e1f9079c8ad96
nateshmbhat <nateshmbhat1@gmail.com>
2017-11-09 19:44:58 +0530

    added the add_rooms func which takes any random room node as param and adds it to the current instance of the node or if the node param is null then it adds permanently to the file.

--- main.c
+++ main.c
@@ -33,8 +33,12 @@ typedef struct rooms
 }ROOM ;
 
 
+
 ///This function is used to add or insert rooms into the pool of rooms and takes the room_no and its floor as input 
 ///Takes any room node as the input and within the function initialises the room_no and floor and flag para
+///This is used whenever newrooms are built and have to be added to the list of rooms 
+
+///If the room parameter is null then the details of the room are read the file and then the added details are appended to the file .
 
 void Add_room(ROOM *room)
 {
@@ -44,27 +48,43 @@ void Add_room(ROOM *room)
 	scanf("%d" , newroom->room_no) ;
 	printf("Enter the floor in which the room exists : ") ; 
 	scanf("%d" , newroom->room_floor) ;
-	
+	newroom->flag_taken = false ;	
+
+
 	if(room==NULL)
 	{
+		///Write the new added room to the room_details . THis file contains each room number and its corresponding floor 
 
-		
+		FILE *fp_room = fopen("rooms_initial.txt" , "a+");
+		if(!fp_room)
+		{
+			printf("\nError opening the rooms_initial.txt file ! \n" );
+			return  ;
+		}
+
+		fprintf(fp_room , "%d %d\n" , newroom->room_no , newroom->room_floor); 
+		fclose(fp_room) ;
 
 	}
 
 	else
 	{
 
+		while(room->next)
+			room = room->next ;
+		room->next = newnode ;
 
 	}
-
-
 }
+
+
+
+
+
 
 
 node * add_student_to_queue(node * first )
 {
-
 
 
 }
